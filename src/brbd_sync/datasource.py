db4cfc7259ed6d147d99712b50ebbd9588775df4
Jeremy Fleischman <jeremyfleischman@gmail.com>
2025-05-19 18:58:35 -0700

    Enforce uniqueness of email address

--- src/brbd_sync/datasource.py
+++ src/brbd_sync/datasource.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 
 from pydantic import BaseModel
 
@@ -9,11 +9,23 @@ class Subscriber(BaseModel):
     metadata: dict[str, str]
 
 
+def unique_group_by[K, V](arr: list[V], key: Callable[[V], K]) -> dict[K, V]:
+    result = {}
+    for v in arr:
+        k = key(v)
+        assert k not in result, f"Unexpected duplicate key: {k}"
+        result[k] = v
+
+    return result
+
+
 class MailingListDatasource(BaseModel):
     subscribers: list[Subscriber]
 
     def model_post_init(self, context: Any):
-        self._subscribers_by_email = {s.email: s for s in self.subscribers}
+        self._subscribers_by_email = unique_group_by(
+            self.subscribers, lambda s: s.email
+        )
 
     def get_subscriber(self, *, email: str) -> Subscriber:
         return self._subscribers_by_email[email]
